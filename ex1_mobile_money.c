03dad9dd852f265ddc21d8185bcdadbbb7c4af48
brianedyabi-svg <brianedyabi@gmail.com>
2026-03-25 01:12:19 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,25 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Edyabi Brian*/
+/* Student Number: 25/U/BIE/05291/PE */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float amount;
+    float trans;
+    float transaction;
 
+    printf ("Enter amount to be sent: ");
+    scanf("%f", &amount);
+
+    printf ("Enter transaction fee: %");
+    scanf ("%f, &trans);
+        transaction = amount * (trans/100.00);
+       amount_deducted = (amount + transaction);
+        
+        printf("Transaction fee:%.2f \n", transaction);
+    printf("amount_deducted:%.2f", amount_deducted);
+    
+    
     return 0;
 }
