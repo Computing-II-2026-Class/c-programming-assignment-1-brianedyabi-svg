fdeefd6c2a30cc0003365efda779d2896856db28
brianedyabi-svg <brianedyabi@gmail.com>
2026-03-25 06:26:54 +0300

    Update ex2_fuel.c

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,11 +1,25 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: Edyabi Brian */
+/* Student Number: 25/U/BIE/05291/PE */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+    float distance_traveled;
+    float fuel_used;
+    float fuel_efficiency;
 
+    printf("Enter distance covered: ");
+    scanf("%f", &distance_traveled);
+
+    print("Enter fuel used: ");
+    scanf("%f", & fuel_uesd);
+
+    fuel_efficiency = (distance_traveled/fuel_used);
+    
+
+    printf("fuel efficiency:%.2f", fuel_efficiency);
+
+    
     return 0;
 }
